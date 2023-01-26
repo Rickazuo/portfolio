6bc3ace381e5e459e0c2d81e172df5a83319f597
Rickazuo <rick3209@gmail.com>
2023-01-26 17:24:39 -0300

    add note carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 
+// Aplicacao do Carousel 
+
 const CarouselBox = () => (
   <Carousel>
     <div className="carousel">
